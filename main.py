53d3bbdcbd4f02ca7872887d46278d812f908c31
Itinii12 <1212965@gmail.com>
2021-08-12 21:31:34 +0900

    add_Dynamodb_test()

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import boto3
 
-def main():
+
+def dynamodb_test():
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("dev-table")
+    response = table.put_item(Item={'k': 'test01', 'date': '2021-8-11'})
+    print(response)
+    print(type(response['ResponseMetadata']))
+    for key, value in response['ResponseMetadata'].items():
+        print(key)
+        print(value)
+
+
+def python_study():
     a = 1
     print(a)
 
 
 if __name__ == "__main__":
-    main()
+    dynamodb_test()
 
